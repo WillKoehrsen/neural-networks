c18db90e2749b0b95efc2cd1a3fc12742f93c176
WillKoehrsen <wjk68@case.edu>
2018-09-05 01:28:42 +0000

    Working

--- taxi_fare/first_cell.py
+++ taxi_fare/first_cell.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 %matplotlib inline
 
-plt.style.use('fivethiryeight')
+plt.style.use('fivethirtyeight')
 plt.rcParams['font.size'] = 18
 
 import seaborn as sns
